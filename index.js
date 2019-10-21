dc72360481c43249b8ce9b85fcec7c02afef3929
saja amer <sajaamer86@gmail.com>
2019-10-21 13:11:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Add your functions here
+function map(arr,fun){
+  let myArr=[] ;
+  for(let i=0;i<arr.length;i++){
+    myArr.push(fun(arr[i]));
+  }
+  return myArr ;
+}
+function reduce(arr,fun,start){
+  let sum=(!!start)?start:arr[0]; 
+  let i=(!!start)?0:1 ;
+  for(;i<arr.length;i++){
+    sum=fun(arr[i],sum);
+  }
+  return sum ;
+}
